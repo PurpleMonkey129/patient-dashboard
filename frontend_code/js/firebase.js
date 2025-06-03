4bbf906e25e5dd7e16c391ed120560d8dee0744b
PurpleMonkey <mjeet129@gmail.com>
2025-06-03 20:48:51 +0530

    Update Firebase configuration for static hosting

--- frontend_code/js/firebase.js
+++ frontend_code/js/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyDtAmUF5c_VY3eUXZQEyKN8oxM_zmj9mOs",
+  authDomain: "patient-dashboard-500d2.firebaseapp.com",
+  projectId: "patient-dashboard-500d2",
+  storageBucket: "patient-dashboard-500d2.firebasestorage.app",
+  messagingSenderId: "178852814723",
+  appId: "1:178852814723:web:b92ae5e5009a1a7e66e0b6"
 };
 
 // Initialize Firebase
